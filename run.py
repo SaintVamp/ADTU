8b78089dd1ca7279ecfd8d1dae0ba27585f460e0
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-11-25 01:36:49 +0000

    通知增加更新说明

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-11-25 09:33:44"
-ts = 1764034424
+ver = "2025-11-25 09:36:48"
+ts = 1764034608
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
