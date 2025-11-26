ef8124b6c3434d1cffbd91f72d00ec5316bf4042
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-11-26 02:29:03 +0000

    修复ptskit的BUG

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-11-26 09:01:31"
-ts = 1764118891
+ver = "2025-11-26 10:29:02"
+ts = 1764124142
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
