2a8c573c977b963e8993f0891237e5f1429b34ee
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-02 09:36:47 +0000

    修复垃圾堆的引用报错

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-02 00:15:49"
-ts = 1764605749
+ver = "2025-12-02 17:36:46"
+ts = 1764668206
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
