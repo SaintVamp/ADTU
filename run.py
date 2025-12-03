91d7c430bf0eb9f873ab8618c1ec0c79d993124b
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-03 08:41:41 +0000

    优化截图报错的问题

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-03 10:24:55"
-ts = 1764728695
+ver = "2025-12-03 16:41:40"
+ts = 1764751300
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
