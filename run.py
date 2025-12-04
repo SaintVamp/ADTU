6ba7a6df2e3d8840ef8d3ca09fb174e7a253f9ec
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-04 00:42:27 +0000

    增加IMAX Enhanced映射

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-04 08:24:41"
-ts = 1764807881
+ver = "2025-12-04 08:42:26"
+ts = 1764808946
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
