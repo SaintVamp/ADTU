f0eb77fd206240c6b4fa7d4e3c3c85f54e2107f8
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-10 14:23:17 +0000

    增加配置重的目录校验

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-10 22:04:17"
-ts = 1765375457
+ver = "2025-12-10 22:23:16"
+ts = 1765376596
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
