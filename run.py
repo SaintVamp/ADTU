e808f417dde82f0f4ec802ae5c6a76ffb7b69c86
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-14 16:07:18 +0000

    增加配置页面

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-11 10:33:38"
-ts = 1765420418
+ver = "2025-12-15 00:07:17"
+ts = 1765728437
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
