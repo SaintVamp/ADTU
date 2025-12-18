f8c3bade7e7da41f60f88da0c7228f84e1f80b12
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-18 04:39:08 +0000

    getHashInfo接口bug修复

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-18 08:42:05"
-ts = 1766018525
+ver = "2025-12-18 12:39:06"
+ts = 1766032746
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
