69cae1aa0f3d5ba148668ccd894d2b032e15fddb
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-19 03:13:13 +0000

    适配猫站规则

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-18 18:50:34"
-ts = 1766055034
+ver = "2025-12-19 11:13:11"
+ts = 1766113991
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
