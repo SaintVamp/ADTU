3d3b5e9f6e29c1844b74d8d9da2d52bcd9b300da
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2025-12-26 17:41:37 +0000

    hdv报错修复&一轮发布全部种子

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-23 22:06:55"
-ts = 1766498815
+ver = "2025-12-27 01:41:36"
+ts = 1766770896
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
