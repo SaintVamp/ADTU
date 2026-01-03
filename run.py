314beac19791210bc745db5eeda0d716da1211b3
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-01-03 11:01:09 +0000

    修改JPG截图问题

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2025-12-28 22:05:09"
-ts = 1766930709
+ver = "2026-01-03 19:01:07"
+ts = 1767438067
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
