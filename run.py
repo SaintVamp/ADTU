903e039d16e416e6b2e5c94e8010d6e2c44d1e52
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-02-06 03:38:47 +0000

    按照HDV顺序要求修改

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-01-30 08:41:13"
-ts = 1769733673
+ver = "2026-02-06 11:38:46"
+ts = 1770349126
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
