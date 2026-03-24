31e996f3e0acb9013077839d79069d48645b4992
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-03-24 05:10:11 +0000

    修复longpt制作组选择

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-02-06 11:38:46"
-ts = 1770349126
+ver = "2026-03-24 13:10:08"
+ts = 1774329008
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
