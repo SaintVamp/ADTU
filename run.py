ff9b689164734c02eb22a5a3a8e6044336b96f07
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-03-25 14:35:56 +0000

    优化luck标签

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-03-24 17:11:35"
-ts = 1774343495
+ver = "2026-03-25 22:35:55"
+ts = 1774449355
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
