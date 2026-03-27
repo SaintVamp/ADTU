3bcbb36ced18335c32c1b9b0b3781ef440df0cf4
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-03-27 12:10:13 +0000

    缺少MT的发布配置

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-03-25 22:35:55"
-ts = 1774449355
+ver = "2026-03-27 20:10:12"
+ts = 1774613412
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
