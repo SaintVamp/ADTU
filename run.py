392bdc35e71837042f53c6b3c3b93c93de27d9e4
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-04-13 10:00:46 +0000

    音频增加A_QUICKTIME适配

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-04-11 21:56:50"
-ts = 1775915810
+ver = "2026-04-13 18:00:45"
+ts = 1776074445
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
