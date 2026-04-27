76b426e26f5682fdafa97b319c6cf99b365df28f
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-04-27 03:13:10 +0000

    财神幸运媒介和财神音频优化

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-04-21 09:24:36"
-ts = 1776734676
+ver = "2026-04-27 11:13:09"
+ts = 1777259589
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
