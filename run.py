b52d492c59e3c5905ee23eb519503ac22f2a1108
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-05-09 12:19:15 +0000

    优化选择高码、高帧逻辑。

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-04-30 11:54:41"
-ts = 1777521281
+ver = "2026-05-09 20:19:13"
+ts = 1778329153
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
