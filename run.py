c5243a0886d7553419a2be6e9af25544f8a1da94
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-05-16 12:08:06 +0000

    菁彩HDR标签优化

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-05-14 11:06:09"
-ts = 1778727969
+ver = "2026-05-16 20:08:05"
+ts = 1778933285
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
