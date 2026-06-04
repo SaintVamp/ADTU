9af4d262707b48c55429faa037a26abff94e8f55
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-06-04 12:02:43 +0000

    幸运增加粤语解析

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-05-16 21:31:19"
-ts = 1778938279
+ver = "2026-06-04 20:02:42"
+ts = 1780574562
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
