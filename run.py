7b3fbaa131992779612ab44ed5e0da1d9eb989a1
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-06-15 07:01:57 +0000

    优化pixhost

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-06-13 09:20:13"
-ts = 1781313613
+ver = "2026-06-15 15:01:56"
+ts = 1781506916
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
