055cb84b918360e12ef24b31be7256562bcd6421
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-06-16 09:05:09 +0000

    青蛙优化中字识别

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-06-15 15:01:56"
-ts = 1781506916
+ver = "2026-06-16 17:05:08"
+ts = 1781600708
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
