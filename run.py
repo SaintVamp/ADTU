af4b78d80be660365adce5342b3e9606a89de6c4
SaintVamp <25300520+saintvamp@users.noreply.github.com>
2026-06-17 22:57:02 +0000

    麒麟动漫选择优化

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from atut_serv import start_serv
 from util.svlog import logs
 
-ver = "2026-06-16 17:05:08"
-ts = 1781600708
+ver = "2026-06-18 06:57:01"
+ts = 1781737021
 if __name__ == '__main__':
     logs.logger.info(f'下载转发端主程序启动，V1.0.1 ver={ver}')
     start_serv(ts)
